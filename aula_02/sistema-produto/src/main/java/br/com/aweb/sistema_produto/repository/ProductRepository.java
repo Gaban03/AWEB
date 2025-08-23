6b84635e98eed90f43bb2d2cd4d751be30e9ee96
Gaban03 <gabanvinicius724@gmail.com>
2025-08-22 22:51:29 -0300

    CRUD com Spring Boot, H2 e MVC

---
+++ aula_02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package br.com.aweb.sistema_produto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.aweb.sistema_produto.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
