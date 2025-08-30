ab71387949ef4d02f1d3fdc80aeb76b41c8705d8
Gaban03 <gabanvinicius724@gmail.com>
2025-08-29 22:46:46 -0300

    Projeto Spring Boot, H2 e MVC, Lombook, DevTools

--- aula_02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/controller/ProductController.java
+++ aula_02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/controller/ProductController.java
@@ -1,5 +1,7 @@
 package br.com.aweb.sistema_produto.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.aweb.sistema_produto.model.Product;
 import br.com.aweb.sistema_produto.service.ProductService;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/products")
@@ -67,4 +70,13 @@ public class ProductController {
         }
         return "redirect:/products";
     }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(required = false) String name, Model model) {
+        if(name != null && !name.isBlank()){
+            model.addAttribute("products", productService.findByName(name));
+        }
+        return "search";
+    }
+
 }
--- aula_02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/repository/ProductRepository.java
+++ aula_02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package br.com.aweb.sistema_produto.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.aweb.sistema_produto.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    public List<Product> findByNameContainingIgnoreCase(String name);
 
 }
